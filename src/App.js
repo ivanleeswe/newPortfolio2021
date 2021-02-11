4890a2998e61e5c3a10f82751daa9c03a0a1a105
Ivan Lee <ilee.code@gmail.com>
2021-02-10 23:59:15 -0800

    Add new files for the carousel component and configure carousel template from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/navbar/navbar";
+import Carousel from "./components/carousel/carousel";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Carousel />
     </div>
   );
 }
