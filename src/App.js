b8109048f845b7bbe2558e3aec25204d308365fc
Ivan Lee <ilee.code@gmail.com>
2021-02-11 01:02:18 -0800

    Add about component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from "./components/navbar/navbar";
 import Carousel from "./components/carousel/carousel";
 import TitleMessage from "./components/title-message/title-message";
+import About from "./pages/about/about";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Carousel />
       <TitleMessage />
+      <About />
     </div>
   );
 }
