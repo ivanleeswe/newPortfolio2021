446946dee34135746184b54e15ffe0feb4e58971
Ivan Lee <ilee.code@gmail.com>
2021-02-11 00:07:02 -0800

    Add title message component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/navbar/navbar";
 import Carousel from "./components/carousel/carousel";
+import TitleMessage from "./components/title-message/title-message";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Carousel />
+      <TitleMessage />
     </div>
   );
 }
